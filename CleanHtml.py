cc37287aaeeda714ebd90e95d3ff05786fc0729d
yudiwbs_desktop_rumah <yudi1975@gmail.com>
2017-05-09 05:53:35 +0700

    direktori

--- CleanHtml.py
+++ CleanHtml.py
@@ -2,16 +2,19 @@ from bs4 import BeautifulSoup
 
 print("bersihkan html")
 
-namaInput = "/home/yudiwbs/lombalazada/data/out_clarity_train_kecil.label0"
-namaOutput = "/home/yudiwbs/lombalazada/data/out_clarity_train_kecil_bersih.label0"
+#namaInput = "/home/yudiwbs/lombalazada/data/out_clarity_train_kecil.label0"
+#namaOutput = "/home/yudiwbs/lombalazada/data/out_clarity_train_kecil_bersih.label0"
+
+namaInput = "/media/yudiwbs/programdata/ubuntu/lombalazada/data/out_clarity_train.label1"
+namaOutput = "/media/yudiwbs/programdata/ubuntu/lombalazada/data/out_clarity_train_bersih.label1"
+
 
 fTeks = open(namaInput)
+
 
 try:
     soup = BeautifulSoup(fTeks, 'lxml')
     teksBersih = soup.get_text()
-    #print(teksBersih)
-
     arrBaris = teksBersih.split("\n")
     for line in arrBaris:
         line = line.rstrip('\n').strip().casefold()
@@ -20,3 +23,6 @@ try:
     #end for
 finally:
     fTeks.close()
+
+
+print("selesai")
